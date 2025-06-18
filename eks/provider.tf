924cbf8fd934fa581e51db41d54fb16daf1913ef
janandababu2023 <136234856+janandababu2023@users.noreply.github.com>
2025-06-18 16:44:44 +0530

    updated provider.tf file

--- eks/provider.tf
+++ eks/provider.tf
@@ -3,9 +3,13 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 data "aws_availability_zones" "available" {}
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
