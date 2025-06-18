707dfe1645e7df621df439620b29f35a6061f203
janandababu2023 <136234856+janandababu2023@users.noreply.github.com>
2025-06-18 19:27:01 +0530

    eks-cluster.tf cloud watch merged into eks module

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,46 +1,33 @@
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
 
-  cluster_name    = "eks-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.29"
 
   cluster_endpoint_public_access = true
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.private_subnets
+
+  cloudwatch_log_group_retention_in_days = 7
+  create_cloudwatch_log_group            = false
 
   eks_managed_node_groups = {
     ubuntu_nodes = {
-      desired_size = 3
-      min_size     = 3
-      max_size     = 3
-
-      instance_types = ["t3.medium"]
-      ami_type       = "CUSTOM"
-
-      ami_id = data.aws_ami.ubuntu.id
+      desired_size    = 3
+      min_size        = 3
+      max_size        = 3
+      instance_types  = ["t3.medium"]
+      ami_type        = "CUSTOM"
+      ami_id          = data.aws_ami.ubuntu.id
 
       labels = {
         role = "worker"
       }
     }
   }
-  module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-  subnet_ids      = module.vpc.public_subnets
-  vpc_id          = module.vpc.vpc_id
-
-  cloudwatch_log_group_retention_in_days = 7
-
-  # Prevent error if log group already exists
-  create_cloudwatch_log_group = false
-}
-
 
   tags = {
     Environment = "dev"
